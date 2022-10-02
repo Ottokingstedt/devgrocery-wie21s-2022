a9ebe1b44e6b6e8d94087988a9ee116a948711e3
SuarPha Kanklap <92083398+SuarPha@users.noreply.github.com>
2022-10-03 00:46:45 +0200

    stylish form
    stylish form

--- themes/devgrocery/index.php
+++ themes/devgrocery/index.php
@@ -29,25 +29,27 @@ get_header();
 			endif;
 
 			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			<?php while ( have_posts() ) :
+				the_post(); ?>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+						<article class="post">
+                        <h3>
+                        <a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h3>
+                        
+                        <?php the_content(); ?>
+                        
+                      
+                    </article>
 
-			endwhile;
+			<?php endwhile; ?>
 
-			the_posts_navigation();
+			<?php the_posts_navigation(); ?>
 
-		else :
+		<?php else : ?>
 
-			get_template_part( 'template-parts/content', 'none' );
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-		endif;
+		<?php endif;
 		?>
 
 	</main><!-- #main -->
