5503ce1e7c447e2df507f57d0c8714c57b5c6912
SuarPha Kanklap <92083398+SuarPha@users.noreply.github.com>
2022-10-02 19:21:38 +0200

    commit update
    commit update

--- themes/devgrocery/footer.php
+++ themes/devgrocery/footer.php
@@ -13,7 +13,34 @@
 
 	<footer id="colophon" class="site-footer">
 
-	<h3>Test footer!</h3>
+	<div class="inner-container">
+			<div class="col-footer">
+				<h3 class="widgettitle">Navigation</h3>
+				<ul>
+					<?php $args = [
+         				'theme_location' => 'footer menu',
+     				]; ?>
+					<?php wp_nav_menu($args); ?>
+				
+				</ul>
+			</div>
+			<div class="col-footer">
+			<h3 class="widgettitle">Information</h3>
+			<ul>
+				<li> <a href="#">Privat Policy</a> </li>
+				<li> <a href="#">Terms</a> </li>
+				<li> <a href="#">Affiliates</a> </li>
+			</ul>
+				
+			</div>
+			<div class="col-footer">
+				
+				<a href="<?php echo get_home_url('/'); ?>"> 
+                <h1><?php bloginfo('name'); ?></h1>
+                </a>
+				
+			</div>
+			</div>	
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
